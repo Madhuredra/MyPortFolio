763720a3adfedd5525114140bf215aa67fa0c422
Madhurendra Nath Tiwari <68775519+Madhuredra@users.noreply.github.com>
2021-12-09 20:32:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,23 +35,7 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-var textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml9 .letter',
-    scale: [0, 1],
-    duration: 1500,
-    elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  }).add({
-    targets: '.ml9',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
   var happy = document.getElementById("Happy");
   function chanhgeEmoji() {
       happy.innerHTML = "🙁"
